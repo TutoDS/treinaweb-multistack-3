e1c5772f6464b6c8d1de543f2ddd6e59987f3f9e
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2022-06-05 06:26:57 +0100

    generate backend (with laravel)

---
+++ backend/database/migrations/2022_06_05_052320_create_pets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name', 100);
+            $table->text('description');
+            $table->string('photo', 1000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pets');
+    }
+};
